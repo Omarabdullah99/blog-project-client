e7b7d913d2dadaeea7b22e20770ee70192a3d73c
Omarabdullah99 <omarabdullah917303@gmail.com>
2023-11-26 22:01:38 +0600

    backend-urladdapp.js

--- src/reudx/api.js
+++ src/reudx/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API= axios.create({baseURL:"http://localhost:5000"})
+const API= axios.create({baseURL:"https://blog-server-last-test.vercel.app"})
 
 //*middleware handle backend and fontend
 //token backedne patabo
